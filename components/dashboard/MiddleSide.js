e5bf09112e0fa62d1ff11a39bc595fa3cd9c374e
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-06-09 18:24:58 +0530

    add right profile component

--- components/dashboard/MiddleSide.js
+++ components/dashboard/MiddleSide.js
@@ -4,10 +4,13 @@ import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
 
 const MiddleSide = () => {
   return (
-    <Stack>
+    <Stack divider={<Divider orientation="horizontal" flexItem />} spacing={2}>
       <TextField
         elevation={2}
         fullWidth
@@ -22,6 +25,11 @@ const MiddleSide = () => {
         }}
         variant="outlined"
       />
+      <Typography variant="h3">
+        Welcome to <span style={{ color: "blue" }}>CarePlus</span>
+      </Typography>
+      <Typography variant="body1">Dashboard</Typography>
+      <Paper elevation="2"></Paper>
     </Stack>
   );
 };
