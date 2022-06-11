c9ae18cf17591f2dfb542ee36e352af2b6511208
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-06-11 13:21:48 +0530

    implement filtering in Doctor.js

--- components/dashboard/doctors/Doctors.jsx
+++ components/dashboard/doctors/Doctors.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useMemo, useCallback, useRef } from "react";
+import { AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+
+import { rowData, columnDefs } from "./doctorsData";
 
 const Doctors = () => {
-  return <div style={{ height: "100vh", width: "100%" }}></div>;
+  const gridRef = useRef();
+  columnDefs.checkboxSelection = () => true;
+
+  const defaultColDef = useMemo(
+    () => ({
+      sortable: true,
+      filter: true,
+    }),
+    []
+  );
+
+  const cellClickedListener = useCallback((event) => {
+    console.log("cellClicked", event);
+  }, []);
+
+  const buttonListener = useCallback((e) => {
+    gridRef.current.api.deselectAll();
+  }, []);
+
+  const getRowClass = (params) => {
+    if (params.node.rowIndex % 2 === 0) {
+      return "my-shaded-effect";
+    }
+  };
+
+  return (
+    <div className="ag-theme-alpine" style={{ height: "80vh", width: "100%" }}>
+      <AgGridReact
+        ref={gridRef}
+        rowData={rowData}
+        columnDefs={columnDefs}
+        defaultColDef={defaultColDef}
+        animateRows={true}
+        rowSelection="multiple"
+        onCellClicked={cellClickedListener}
+        rowClass="my-green-class"
+        getRowClass={getRowClass}
+        checkboxSelection={true}
+      ></AgGridReact>
+    </div>
+  );
 };
 
 export default Doctors;
