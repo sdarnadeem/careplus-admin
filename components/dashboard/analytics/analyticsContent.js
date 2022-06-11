afd03d7cd0676304fe7e437d710737b974d67938
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-06-11 16:26:38 +0530

    implement appointment tab in analytics/ this month appointment

--- components/dashboard/analytics/analyticsContent.js
+++ components/dashboard/analytics/analyticsContent.js
@@ -415,6 +415,162 @@ const userRows = [
   },
 ];
 
+const appointmentColumns = [
+  {
+    field: "id",
+    headerName: "ID",
+    filter: true,
+    filterParams: textFilterParams,
+  },
+  {
+    field: "firstName",
+    headerName: "First Name",
+    filter: true,
+    filterParams: textFilterParams,
+  },
+  {
+    field: "lastName",
+    headerName: "Last Name",
+    filter: true,
+    filterParams: textFilterParams,
+  },
+  {
+    field: "address",
+    headerName: "Address",
+    filter: true,
+    filterParams: textFilterParams,
+  },
+  {
+    field: "appointmentDate",
+    headerName: "Appointment Date",
+    filter: true,
+    filterParams: textFilterParams,
+  },
+  {
+    field: "doctor",
+    headerName: "Doctor",
+    filter: true,
+    filterParams: textFilterParams,
+  },
+  {
+    field: "clinic",
+    headerName: "Clinic",
+    filter: true,
+    filterParams: textFilterParams,
+  },
+];
+
+const appointmentRows = [
+  {
+    id: "154",
+    firstName: "ALbert",
+    lastName: "Helski",
+    address: "Sopore, Sagipora",
+    appointmentDate: "23-June-2023",
+    doctor: "Alaxnder",
+    clinic: "Summer Hall",
+  },
+  {
+    id: "990",
+    firstName: "Cersei",
+    lastName: "Twain",
+    address: "Sopore, Sagipora",
+    appointmentDate: "31-July-2020",
+    doctor: "Nadeem",
+    clinic: "Tripmax",
+  },
+  {
+    id: "224",
+    firstName: "Jaime",
+    lastName: "Twain",
+    address: "Sopore, Sagipora",
+    appointmentDate: "31-July-2020",
+    doctor: "Nadeem",
+    clinic: "Tripmax",
+  },
+  {
+    id: "334",
+    firstName: "Arya",
+    lastName: "Twain",
+    address: "Sopore, Sagipora",
+    appointmentDate: "31-July-2020",
+    doctor: "Nadeem",
+    clinic: "Tripmax",
+  },
+  {
+    id: "440",
+    firstName: "Daenerys",
+    lastName: "Twain",
+    address: "Sopore, Sagipora",
+    appointmentDate: "31-July-2020",
+    doctor: "Nadeem",
+    clinic: "Tripmax",
+  },
+  {
+    id: "370",
+    firstName: "Hommie",
+    lastName: "Twain",
+    address: "Sopore, Sagipora",
+    appointmentDate: "31-July-2020",
+    doctor: "Nadeem",
+    clinic: "Tripmax",
+  },
+  {
+    id: "284",
+    firstName: "Ferrara",
+    lastName: "Twain",
+    address: "Sopore, Sagipora",
+    appointmentDate: "31-July-2020",
+    doctor: "Nadeem",
+    clinic: "Tripmax",
+  },
+  {
+    id: "775",
+    firstName: "Rossini",
+    lastName: "Twain",
+    address: "Sopore, Sagipora",
+    appointmentDate: "31-July-2020",
+    doctor: "Nadeem",
+    clinic: "Tripmax",
+  },
+  {
+    id: "980",
+    firstName: "Harvey",
+    lastName: "Twain",
+    address: "Sopore, Sagipora",
+    appointmentDate: "31-July-2020",
+    doctor: "Nadeem",
+    clinic: "Tripmax",
+  },
+  {
+    id: "354",
+    firstName: "Harvey",
+    lastName: "Twain",
+    address: "Sopore, Sagipora",
+    appointmentDate: "31-July-2020",
+    doctor: "Nadeem",
+    clinic: "Tripmax",
+  },
+  {
+    id: "79",
+    firstName: "Harvey",
+    lastName: "Twain",
+    address: "Sopore, Sagipora",
+    appointmentDate: "31-July-2020",
+    doctor: "Nadeem",
+    clinic: "Tripmax",
+  },
+  {
+    id: "29",
+    firstName: "Harvey",
+    lastName: "Twain",
+    address: "Sopore, Sagipora",
+    appointmentDate: "31-July-2020",
+    doctor: "Nadeem",
+    clinic: "Tripmax",
+  },
+];
+
 const data = [
   {
     rows: clinicRows,
@@ -427,6 +583,10 @@ const data = [
   {
     rows: userRows,
     columns: userColumns,
+  },
+  {
+    rows: appointmentRows,
+    columns: appointmentColumns,
   },
 ];
 
