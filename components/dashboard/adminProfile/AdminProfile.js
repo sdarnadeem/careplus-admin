132b0dd9330bb81d156e2d57c3800d470680322a
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-06-15 11:58:12 +0530

    implement admin page top bar

--- components/dashboard/adminProfile/AdminProfile.js
+++ components/dashboard/adminProfile/AdminProfile.js
@@ -1,7 +1,30 @@
 import React from "react";
 
+import { Grid, Button, Typography, Divider } from "@mui/material";
+
 const AdminProfile = () => {
-  return <div>AdminProfile</div>;
+  return (
+    <>
+      <Grid container spacing={2} justifyContent="flex-end">
+        <Grid item marginRight="auto">
+          <Typography variant="h5" component="h5">
+            Admin Profile
+          </Typography>
+        </Grid>
+        <Grid item height="100%" alignItems="center">
+          <Button variant="outlined" size="small">
+            Update Admin
+          </Button>
+        </Grid>
+        <Grid item height="100%" alignItems="center">
+          <Button variant="outlined" size="small">
+            Update Password
+          </Button>
+        </Grid>
+      </Grid>
+      <Divider />
+    </>
+  );
 };
 
 export default AdminProfile;
